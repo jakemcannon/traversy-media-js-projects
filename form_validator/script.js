87b146ad826f734e5bf49b42b46bd355135a89ae
Jake Cannon <jakecan13@gmail.com>
2020-02-26 20:34:19 -0500

    refactor for a few edge cases and complete

--- form_validator/script.js
+++ form_validator/script.js
@@ -22,11 +22,17 @@ function showSuccess(input) {
 }
 
 // check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
   return re.test(String(email).toLowerCase());
 }
 
+// check required fields
 function checkRequired(inputArr) {
   inputArr.forEach(function(input) {
     if (input.value.trim() === "") {
@@ -35,6 +41,24 @@ function checkRequired(inputArr) {
       showSuccess(input);
     }
   });
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if(input.value.length < min) {
+    showError(input, `get${getFieldName(input)} must be at least ${min}`);
+  } else if(input.value.length > max) {
+    showError(input, `get${getFieldName(input)} must be less than ${max}`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value != input2.value) {
+    showError(input2, 'passwords do not match');
+  }
 }
 
 // Get fieldname
@@ -46,4 +70,8 @@ form.addEventListener("submit", function(e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
